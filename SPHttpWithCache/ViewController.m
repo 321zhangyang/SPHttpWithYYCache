1a36cfdae4bd3d4b5022c30ed796a3f3d7b5b91f
换一换 <huanyihuan@huanyihuandeMacBook-Pro.local>
2016-03-08 15:03:38 +0800

    @AF3.0网络封装 YYCache 做缓存

--- SPHttpWithCache/ViewController.m
+++ SPHttpWithCache/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "SPHttpWithYYCache.h"
 @interface ViewController ()
 
 @end
@@ -17,8 +17,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+ // [self deleteCache];
+   [SPHttpWithYYCache getRequestCacheUrlStr:@"http://api.beikeshushe.com/gateway.php??uid=33&method=readgroup.recommend_read_group&p=1" success:^(NSDictionary *requestDic, NSString *msg) {
+       NSLog(@"%@",requestDic);
+   } failure:^(NSString *errorInfo) {
+       
+   }];
+    
+    
+    
 }
-
+-(void)deleteCache
+{
+    [[[YYCache alloc] initWithName:@"SPHttpCache"] removeAllObjectsWithProgressBlock:nil endBlock:^(BOOL error) {
+        if (!error) {
+            NSLog(@"清除成功");
+        }
+    }];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
